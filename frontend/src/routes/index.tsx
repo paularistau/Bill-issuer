a39dcd12cda89ca8f3da9d0b0d0de4fa76762265
Paula Ristau <paularistau@Paulas-MacBook-Air.local>
2023-04-30 06:46:41 -0300

    fix: running without problems

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes as Switch,
+} from 'react-router-dom';
+import { Payments } from '../pages/Payments';
+import { Debits } from '../pages/Debits';
+
+function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/payments">
+          <Payments />
+        </Route>
+        <Route path="/debits">
+          <Debits />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default Routes;
